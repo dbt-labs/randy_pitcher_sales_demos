c3eb93f89ae16fe0d35ed7f7eb5cdbd74ba7ebcb
Randy <randy.pitcher@fishtownanalytics.com>
2022-02-02 20:08:10 +0000

    in progres

--- tests/monthly_gross_revenue__assert_fresh_data.sql
+++ tests/monthly_gross_revenue__assert_fresh_data.sql
@@ -1,10 +1,13 @@
-{{ config(severity='warn') }}
+{{ config(severity='warn', store_failures=true) }}
 
 
 select 
-    max(ORDER_MONTH)                             as latest_month,
+    '{{invocation_id}}' as invocation_id,
+    ORDER_MONTH                             as latest_month,
     date_trunc('month', current_timestamp::date) as current_month
 
 from {{ref('monthly_gross_revenue')}}
 
-having latest_month != current_month
+where latest_month != current_month
+
+limit 1
