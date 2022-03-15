1e6109bf34f8fe7c391e44bfd5491fb9b502b0ce
Randy <randy.pitcher@fishtownanalytics.com>
2022-03-15 15:44:41 +0000

    added region and fixed tests

--- models/core/hello_world/my_first_model.sql
+++ models/core/hello_world/my_first_model.sql
@@ -2,6 +2,6 @@
 
 select 
     'Hello, World!' as my_first_col, 
-    1/1 as not_a_mistake,
-    current_timestamp as created_at
-
+    1/2 as not_a_mistake,
+    current_timestamp as created_at,
+    ({{to_datestring(created_at)}}) as created_at_str
