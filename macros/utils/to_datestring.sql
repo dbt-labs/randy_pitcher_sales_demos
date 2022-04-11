9690672df619df3a1bd5c141fb0b7d347e4eddca
Randy Pitcher <randy.pitcher@fishtownanalytics.com>
2022-04-11 19:01:48 +0000

    added better macro and demo

--- macros/utils/to_datestring.sql
+++
@@ -1,7 +0,0 @@
-{% macro to_datestring(date_col) %}
-    
-    EXTRACT(MONTH FROM {{date_col}})::STRING || '/' || 
-    EXTRACT(DAY FROM {{date_col}})::STRING || '/' || 
-    EXTRACT(YEAR FROM {{date_col}})::STRING
-
-{% endmacro %}
