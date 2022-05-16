535198d3547148b6cc1c09074faa0254cfff4412
Randy Pitcher <randy.pitcher@fishtownanalytics.com>
2022-05-16 14:34:41 +0000

    changed a value

--- models/core/hello_world/my_first_model.sql
+++ models/core/hello_world/my_first_model.sql
@@ -1,18 +1,6 @@
--- {{target.name}}
--- {{target.warehouse}}
--- {{target.role}}
--- {{target.schema}}
--- {{target.database}}
--- {{this.database}}
-
-{{
-    config(
-        schema='my_first_schema'
-    )
-}}
 
 select 
     'Hello, World!' as my_first_col, 
-    1/1 as not_a_mistake,
+    1/2 as not_a_mistake,
     current_timestamp as created_at
 
