32234fe0a4939b5b23d6abd859cacd6b0fb21acf
Randy <randypitcherii@gmail.com>
2022-05-16 18:24:22 -0400

    did some cleanup (#31)
    * did some cleanup
    
    * fixed actual bug breaking PRs
    
    Co-authored-by: Randy Pitcher <randy.pitcher@fishtownanalytics.com>

--- models/core/hello_world/my_first_model.sql
+++ models/core/hello_world/my_first_model.sql
@@ -1,15 +1,3 @@
--- {{target.name}}
--- {{target.warehouse}}
--- {{target.role}}
--- {{target.schema}}
--- {{target.database}}
--- {{this.database}}
-
-{{
-    config(
-        schema='my_first_schema'
-    )
-}}
 
 select 
     'Hello, World!' as my_first_col, 
