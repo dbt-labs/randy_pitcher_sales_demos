35b00023873daf125d7ceede508c73cef238a752
Randy Pitcher <randy.pitcher@fishtownanalytics.com>
2022-06-29 20:39:33 +0000

    fixed broken key

--- models/staging/tpch/stg_tpch_orders.sql
+++ models/staging/tpch/stg_tpch_orders.sql
@@ -4,7 +4,8 @@ with source as (select * from {{ source('tpch', 'orders') }}),
 RENAMED AS (
     SELECT
     
-        O_ORDERKEY AS ORDER_KEYYYYYY,
+        O_ORDERKEY AS ORDER_KEY,
+        O_ORDERKEY AS ORDER_KEYyyyyyyyy,
         O_CUSTKEY AS CUSTOMER_KEY,
         O_ORDERSTATUS AS STATUS_CODE,
         O_TOTALPRICE AS TOTAL_PRICE,
