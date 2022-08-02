255341984d86f991b17ea218910153ad71a91934
Randy <randy.pitcher@fishtownanalytics.com>
2022-08-02 00:38:41 +0000

    finished staging layer

--- models/retail/staging/stg_part_suppliers.sql
+++ models/retail/staging/stg_part_suppliers.sql
@@ -1,5 +1,5 @@
 select
-    {{ dbt_utils.surrogate_key(['part_key', 'supplier_key']) }} as part_supplier_key,
+    {{ dbt_utils.surrogate_key(['ps_partkey', 'ps_suppkey']) }} as part_supplier_key,
     ps_partkey                                                  as part_key,
     ps_suppkey                                                  as supplier_key,
     ps_availqty                                                 as available_quantity,
