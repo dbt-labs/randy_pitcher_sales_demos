f30c27ed1fb649d21e05d0fd4af1fa6f54639d74
Randy <randy.pitcher@fishtownanalytics.com>
2022-10-14 15:22:27 +0000

    updated schema logic

--- macros/config/generate_schema_name.sql
+++ macros/config/generate_schema_name.sql
@@ -1,13 +1,14 @@
 {%- macro generate_schema_name(custom_schema_name, node) -%}
+    {% set default_schema = env_var('DBT_DEFAULT_SCHEMA') %}
 
     {% if target.name == 'default' %}
-        {{target.schema}}{{ '_' ~ custom_schema_name if custom_schema_name else '' }}
+        {{target.schema}}__{{default_schema}}
 
     {% elif target.name == 'pr_testing' %}
-        {{target.schema}}{{ '_' ~ custom_schema_name if custom_schema_name else '' }}   
+        {{target.schema}}__{{default_schema}}
 
     {% elif 'production' in target.name %}
-        {{ custom_schema_name if custom_schema_name else target.schema }}
+        {{ custom_schema_name if custom_schema_name else default_schema }}
         
     {% else %}
         {%- set err_msg -%}
