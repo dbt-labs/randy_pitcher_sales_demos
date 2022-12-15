f6e4e590e62a4bf93ce8425323335866bdee8749
Randy <randy.pitcher@fishtownanalytics.com>
2022-12-15 17:55:35 +0000

    fixed some stuff

--- models/retail/staging/stg_regions.sql
+++ models/retail/staging/stg_regions.sql
@@ -1,6 +1,6 @@
 select
     {{dbt_utils.surrogate_key(['r_name', 'r_comment'])}} as surrogate_key,
-    r_regionkey as region_keyy,
+    r_regionkey as region_key,
     r_name      as name,
     r_comment   as comment
 
---
+++ models/retail/uat/__monthly_gross_revenue__uat.sql
@@ -0,0 +1,14 @@
+
+select
+    region,
+    date_trunc(month, order_date) as order_month,
+    sum(gross_item_sales_amount)  as gross_revenue
+
+from 
+    {{ref('fct_order_items')}}
+
+group by 
+    region, order_month
+
+order by 
+    order_month, region
