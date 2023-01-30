a5d84480b97d1845bc5ffc9cb6ace425a5b7572b
Randy <randy.pitcher@fishtownanalytics.com>
2023-01-30 20:49:03 +0000

    added region

--- models/retail/marts/monthly_gross_revenue.sql
+++ models/retail/marts/monthly_gross_revenue.sql
@@ -1,16 +1,17 @@
 
 select
     date_trunc(month, order_date) as order_month,
+    region,
     sum(gross_item_sales_amount)  as gross_revenue
 
 from 
     {{ref('fct_order_items')}}
 
 group by 
-    order_month
+    order_month, region
 
 order by 
-    order_month
+    order_month, region
 
 
 
