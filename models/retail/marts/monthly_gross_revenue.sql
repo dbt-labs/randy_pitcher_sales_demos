775a494926cc1184430803609ae8bfd66d257277
Randy <randy.pitcher@fishtownanalytics.com>
2023-01-31 15:29:13 +0000

    added region and nation

--- models/retail/marts/monthly_gross_revenue.sql
+++ models/retail/marts/monthly_gross_revenue.sql
@@ -1,12 +1,12 @@
 
 select
-    order_month,
+    order_month, nation, region,
     sum(gross_revenue) as gross_revenue
 
 from {{ref('order_details')}}
 
-group by order_month
-order by order_month desc
+group by order_month, nation, region
+order by order_month desc, gross_revenue desc
 
 
 
