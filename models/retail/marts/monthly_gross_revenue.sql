3985e6e5fab9cc983cdfd58dceeea62b848338af
Randy <randypitcherii@gmail.com>
2023-01-30 22:34:46 -0500

    Feature/jan2023 demo upgrade (#106)
    * cleanup
    
    * spiffed up the deps
    
    * tidying
    
    * EVERYTHING LOOKS GOOD NOW
    
    * cleaner lineage
    
    * added some interesting behavior out of canada and algeria
    
    * reset the demo
    
    * cleaned up the star of the show by moving month trunc logic into order_details and out of monthly_gross_revenue
    
    ---------
    
    Co-authored-by: Randy <randy.pitcher@fishtownanalytics.com>

--- models/retail/marts/monthly_gross_revenue.sql
+++ models/retail/marts/monthly_gross_revenue.sql
@@ -1,16 +1,12 @@
 
 select
-    date_trunc(month, order_date) as order_month,
-    sum(gross_revenue)            as gross_revenue
+    order_month,
+    sum(gross_revenue) as gross_revenue
 
-from 
-    {{ref('order_details')}}
+from {{ref('order_details')}}
 
-group by 
-    order_month
-
-order by 
-    order_month desc
+group by order_month
+order by order_month desc
 
 
 
