0f73d04ecd4d8367b9d8d9196d7c389cbf9efabe
Randy <randy.pitcher@fishtownanalytics.com>
2023-01-31 03:23:21 +0000

    added some interesting behavior out of canada and algeria

--- models/retail/marts/monthly_gross_revenue.sql
+++ models/retail/marts/monthly_gross_revenue.sql
@@ -1,16 +1,17 @@
 
 select
     date_trunc(month, order_date) as order_month,
-    sum(gross_item_sales_amount)  as gross_revenue
+    sum(gross_revenue)            as gross_revenue,
+    region, nation
 
 from 
     {{ref('order_details')}}
 
 group by 
-    order_month
+    order_month, region, nation
 
 order by 
-    order_month
+    order_month desc, gross_revenue desc
 
 
 
