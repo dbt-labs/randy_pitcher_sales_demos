abd14c15ea686c541aa2822a8cb50eb5481c35ed
Randy <randy.pitcher@fishtownanalytics.com>
2023-01-31 18:04:16 +0000

    removed target.name usage

--- macros/config/generate_schema_name.sql
+++ macros/config/generate_schema_name.sql
@@ -1,15 +1,16 @@
 {%- macro generate_schema_name(custom_schema_name, node) -%}
-    {% set default_schema = env_var('DBT_DEFAULT_SCHEMA') %}
+    {% set current_environment = env_var('DBT_ENVIRONMENT_NAME') %}
+    {% set default_schema      = env_var('DBT_DEFAULT_SCHEMA') %}
 
-    {% if target.name == 'default' or 'ci' in target.name.lower() or 'test' in target.name.lower() %}
+    {% if current_environment == 'default' or 'ci' in current_environment.lower() or 'test' in current_environment.lower() %}
         {{target.schema}}
 
-    {% elif 'production' in target.name %}
+    {% elif 'production' in current_environment %}
         {{ custom_schema_name if custom_schema_name else default_schema }}
         
     {% else %}
         {%- set err_msg -%}
-            Unrecognized target.name provided - Got: {{target.name}}
+            Unrecognized current_environment provided - Got: {{current_environment}}
                 node.identifier: {{node.identifier}}
                 custom_schema_name: {{custom_schema_name}}
                 node: {{node}}
