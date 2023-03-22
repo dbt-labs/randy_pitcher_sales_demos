c31483443879201f6fab558303c0f3abff41b207
Randy <randy.pitcher@fishtownanalytics.com>
2023-03-22 16:44:35 +0000

    added more dimensions to montly rev

--- models/retail/marts/monthly_revenue.sql
+++ models/retail/marts/monthly_revenue.sql
@@ -1,11 +1,17 @@
 
 select
     order_month,
+    nation,
+    region,
+    customer_market_segment,
     sum(gross_revenue) as total_revenue
 
 from {{ref('order_details')}}
 
-group by order_month
+group by order_month,
+    nation,
+    region,
+    customer_market_segment
 order by order_month desc
 
 
