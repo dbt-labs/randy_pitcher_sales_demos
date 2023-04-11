d123f71d28a494a788c34d15c5e4ec104e38a3ca
Randy <randy.pitcher@fishtownanalytics.com>
2023-04-11 18:01:29 +0000

    fixed monthly dashboard

--- models/retail/marts/monthly_revenue.sql
+++ models/retail/marts/monthly_revenue.sql
@@ -1,12 +1,20 @@
 
 select
     order_month,
+    nation,
+    region,
+    customer_market_segment,
     sum(gross_revenue) as total_revenue
 
 from {{ref('order_details')}}
 
-group by order_month
-order by order_month desc
+group by order_month,nation,
+    region,
+    customer_market_segment
+order by order_month desc,
+    nation,
+    region,
+    customer_market_segment
 
 
 
