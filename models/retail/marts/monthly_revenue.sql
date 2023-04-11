a51d1fb19c925b64b29b9754f95bc24f549f8039
Randy <randypitcherii@gmail.com>
2023-04-11 14:27:31 -0400

    Revert "fixed monthly dashboard (#119)"
    This reverts commit 1d255d3aa26ed3c81d40c9ee9c4ff289c769e07c.

--- models/retail/marts/monthly_revenue.sql
+++ models/retail/marts/monthly_revenue.sql
@@ -1,20 +1,12 @@
 
 select
     order_month,
-    nation,
-    region,
-    customer_market_segment,
     sum(gross_revenue) as total_revenue
 
 from {{ref('order_details')}}
 
-group by order_month,nation,
-    region,
-    customer_market_segment
-order by order_month desc,
-    nation,
-    region,
-    customer_market_segment
+group by order_month
+order by order_month desc
 
 
 
