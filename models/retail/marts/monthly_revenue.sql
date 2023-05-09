ab5b15c631117e857ff8bb317f7e676a88c26dca
Randy <randy.pitcher@fishtownanalytics.com>
2023-05-09 17:49:44 +0000

    added customer market segment to monthly revenue

--- models/retail/marts/monthly_revenue.sql
+++ models/retail/marts/monthly_revenue.sql
@@ -1,11 +1,11 @@
 
 select
-    order_month,
+    order_month, customer_market_segment,
     sum(gross_revenue) as total_revenue
 
 from {{ref('order_details')}}
 
-group by order_month
+group by order_month, customer_market_segment
 order by order_month desc
 
 
