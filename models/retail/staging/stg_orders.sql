bcd6f09a7285c1ce09e14e5ec38d0881a8b4cc7e
Randy <randy.pitcher@fishtownanalytics.com>
2023-05-10 19:37:26 +0000

    added simulated user traffic to stg_orders

--- models/retail/staging/stg_orders.sql
+++ models/retail/staging/stg_orders.sql
@@ -8,9 +8,13 @@ select
     o_clerk         as clerk_name,
     o_shippriority  as ship_priority,
 
-    dateadd('hour', 21, order_date) as traffic_peak, -- 21:00 UTC aka 2pm PT
-    dateadd('minute', round(gaussian(0, 60) * power(-1, round(random()))), traffic_peak) as traffic_at,
-    dateadd('hour', 1, date_trunc('hour', traffic_at)) as loaded_at
+
+    -- Let's simulate some live user traffic below from these orders.
+    uniform(0, 7, random()) as days_of_traffic_window,
+    dateadd('day', -days_of_traffic_window, current_date) as traffic_date,
+    dateadd('hour', 21, traffic_date) as traffic_peak, -- 21:00 UTC aka 2pm PT
+    dateadd('minute', normal(0, 60*4, random()), traffic_peak) as traffic_at, -- normal distribution around the traffic peak with stdev=4 hour
+    dateadd('hour', 1, date_trunc('hour', traffic_at)) as loaded_at -- simulated loaded_at at the top of the hour from when traffic was simulated
 
 from 
     {{ source('retail', 'orders') }}
