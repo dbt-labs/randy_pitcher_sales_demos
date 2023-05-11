3b2f7fa20981e286fa50165b81a0f4def3599a04
Randy <randypitcherii@gmail.com>
2023-05-10 21:44:44 -0400

    Feature/data quality event (#124)
    * - updated groups
    - updated retail sources with source freshness
    
    * - pulled exposures into a named yaml
    - revenue now points to sigma
    
    * making progress on data quality demo
    
    * added simulated user traffic to stg_orders
    
    * generating suitable simulated traffic data in stg_orders
    
    * finished bad_timing
    
    * moved the simulated traffic out of stg_orders and into its own space - this is obvious in retrospect haha
    
    * small file shuffleabout + simulated customer service region casing issue
    
    * made the customer support simulated data more obvious and simplified
    
    * finished bad data
    
    * added junk to overcount the MACHINERY market segment - since they order more parts and all
    
    * added a sliding last 6 months filter to market segment reporting
    
    * added application_traffic assertion for frequent data (more than 10 entries per day over the last three days
    
    ---------
    
    Co-authored-by: Randy <randy.pitcher@fishtownanalytics.com>

--- models/retail/modeled/order_details.sql
+++ models/retail/modeled/order_details.sql
@@ -8,15 +8,14 @@ select
     items.net_item_sales_amount,
 
     customers.customer_key,
-    customers.name as customer_name,
-    customers.nation as nation,
-    customers.region as region,
+    customers.name           as customer_name,
+    customers.nation         as nation,
+    customers.region         as region,
     customers.market_segment as customer_market_segment,
 
-    current_date - order_date as num_days_since_order,
-    iff(num_days_since_order <= 180, 1 + 10*num_days_since_order/180, 1) as rev_multiplier,
-    nation = 'CANADA' or nation = 'ALGERIA' as should_multiply_revenue,
-    iff(should_multiply_revenue, net_item_sales_amount * rev_multiplier, net_item_sales_amount)::int as gross_revenue
+
+    -- let's simulate some bad data by double counting certain market segments
+    case when customer_market_segment = 'MACHINERY' then items.total_price else net_item_sales_amount end as gross_revenue
 
 
 from
---
+++ tests/assert_frequent_application_traffic.sql
@@ -0,0 +1,17 @@
+{{
+    config(
+        severity='warn'
+    )
+}}
+
+select 
+    HLL(traffic_time)  as approx_distinct_times, -- hll = hyper log log = aproximate distinct count function
+    traffic_time::date as traffic_date
+    
+from {{ ref('application_traffic') }}
+
+where traffic_date >= dateadd('day', -3, current_date)
+
+group by traffic_date
+
+having approx_distinct_times < 10 
