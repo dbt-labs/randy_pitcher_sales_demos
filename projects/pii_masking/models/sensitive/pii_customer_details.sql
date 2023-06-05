79e7329b10dbadbb6a6e49f82c74c075dd30a091
Randy <randy.pitcher@fishtownanalytics.com>
2023-06-05 23:18:34 +0000

    added more content to the pii tables

--- projects/pii_masking/models/sensitive/pii_customer_details.sql
+++ projects/pii_masking/models/sensitive/pii_customer_details.sql
@@ -1,1 +1,9 @@
-select 1 as ssn, 'Randy' as name, 'Hitsville, USA' as city
+select 
+    1231231234 as ssn, 
+    'Randy' as first_name,
+    'Danger' as middle_name,
+    'Pitcher' as last_name,
+    'Motown' as city, 
+    'MI' as state,
+    '867-5309' as phone,
+    'pisces' as zodiac
