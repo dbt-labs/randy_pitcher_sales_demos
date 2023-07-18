93b4046a4aabf338a0ba56d5be4f4d084b447acd
Randy Pitcher <randy.pitcher@fishtownanalytics.com>
2023-07-18 19:30:07 +0000

    removed all sub projects

--- models/data_quality/bad_data/customer_support_demand_by_region.sql
+++
@@ -1,9 +0,0 @@
-select 
-    support_date,
-    region
-
-from {{ ref('stg_customer_support_activity') }}
-
-where 
-    -- filter to the top 5 regions for headcount planning report
-    region in ('AMERICAS', 'EMEA', 'APAC')
