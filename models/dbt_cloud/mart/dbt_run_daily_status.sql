d9feecc9eea177ea2f8f0534214360c3678634b2
Randy Pitcher <randy.pitcher@fishtownanalytics.com>
2023-07-30 23:15:50 +0000

    added mart to understand run status over time

--- models/dbt_cloud/mart/dbt_run_daily_status.sql
+++ models/dbt_cloud/mart/dbt_run_daily_status.sql
@@ -1,10 +1,11 @@
 select 
     to_timestamp_ntz(run_created_at)::date as created_at_date,
-    run_status_humanized,
+    run_status_humanized::string as run_status,
     job_name,
     environment_name,
     run_environment_id,
-    count(run_id)
+    count(run_id) as run_count,
+    avg(run_duration_in_seconds) as run_avg_duration
 
 from {{ ref('fct_dbt_cloud_job_runs') }}
 
--- models/dbt_cloud/modeled/fct_dbt_cloud_job_runs.sql
+++ models/dbt_cloud/modeled/fct_dbt_cloud_job_runs.sql
@@ -1,6 +1,12 @@
 select 
     runs.*,
-    jobs.* exclude job_id
+    jobs.* exclude job_id,
+
+    TO_TIME(run_duration) AS run_duration_formatted,
+    EXTRACT(HOUR FROM run_duration_formatted) * 3600 
+     + EXTRACT(MINUTE FROM run_duration_formatted) * 60
+     + EXTRACT(SECOND FROM run_duration_formatted) AS run_duration_in_seconds
+
 
 from 
     {{ ref('stg_dbt_cloud_job_runs') }} runs left outer join {{ ref('dim_dbt_cloud_jobs') }} jobs
