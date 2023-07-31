b4b0a1535ae94bd3428211d1d990866456f4cc8f
Randy <randypitcherii@gmail.com>
2023-07-31 14:02:53 -0400

    added an error (#143)
    * added an error
    
    * louder error
    
    ---------
    
    Co-authored-by: Randy Pitcher <randy.pitcher@fishtownanalytics.com>

--- models/dbt_cloud/marts/dbt_run_daily_status.sql
+++ models/dbt_cloud/marts/dbt_run_daily_status.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
 select 
     to_timestamp_ntz(run_created_at)::date as created_at_date,
     run_status_humanized::string as run_status,
@@ -5,7 +11,8 @@ select
     environment_name,
     run_environment_id,
     count(run_id) as run_count,
-    avg(run_duration_in_seconds) as run_avg_duration
+    avg(run_duration_in_seconds) as run_avg_duration,
+    1/0 as err
 
 from {{ ref('fct_dbt_cloud_job_runs') }}
 
