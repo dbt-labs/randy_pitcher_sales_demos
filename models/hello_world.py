4de894f1ef67611ca79d13298cbc4df1e00c8d28
Randy <randypitcherii@gmail.com>
2023-08-30 15:02:22 -0400

    Revert "Demo/2023 08 30  2 (#148)" (#149)
    This reverts commit 9ca4c882c18a15e540df39ab340a4dd8299d30ec.

--- models/hello_world.py
+++
@@ -1,5 +0,0 @@
-def model(dbt, session):
-    dbt.config(materialized = "table")
-    df = dbt.ref("stg_customers")
-
-    return df
--- models/retail/marts/monthly_revenue.sql
+++ models/retail/marts/monthly_revenue.sql
@@ -1,14 +1,11 @@
 
 select
     order_month,
-    nation,
-    region,
-    customer_market_segment,
     sum(gross_revenue) as total_revenue
 
 from {{ref('order_details')}}
 
-group by all
+group by order_month
 order by order_month desc
 
 
