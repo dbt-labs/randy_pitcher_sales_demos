9ca4c882c18a15e540df39ab340a4dd8299d30ec
Randy <randypitcherii@gmail.com>
2023-08-30 14:51:43 -0400

    Demo/2023 08 30  2 (#148)
    * updated monthly revenue
    
    * added python
    
    ---------
    
    Co-authored-by: Randy Pitcher <randy.pitcher@fishtownanalytics.com>

--- models/retail/marts/monthly_revenue.sql
+++ models/retail/marts/monthly_revenue.sql
@@ -1,11 +1,14 @@
 
 select
     order_month,
+    nation,
+    region,
+    customer_market_segment,
     sum(gross_revenue) as total_revenue
 
 from {{ref('order_details')}}
 
-group by order_month
+group by all
 order by order_month desc
 
 
--- models/retail/modeled/order_details.sql
+++ models/retail/modeled/order_details.sql
@@ -2,24 +2,23 @@ select
     items.order_item_key,
     items.order_key,
     items.order_date,
-        date_trunc(month, items.order_date) as order_month,
+    date_trunc(month, items.order_date) as order_month,
     items.return_flag,
     items.line_number,
     items.net_item_sales_amount,
     items.total_price,
 
     customers.customer_key,
-    customers.name           as customer_name,
-    customers.nation         as nation,
-    customers.region         as region,
+    customers.name as customer_name,
+    customers.nation as nation,
+    customers.region as region,
     customers.market_segment as customer_market_segment,
 
-
     -- let's simulate some bad data by double counting certain market segments
-    customer_market_segment = 'MACHINERY' and order_date >= dateadd(day, -100, current_date) as should_miscalculate,
+    customer_market_segment = 'MACHINERY'
+    and order_date >= dateadd(day, -100, current_date) as should_miscalculate,
     iff(should_miscalculate, items.total_price, net_item_sales_amount) as gross_revenue
 
-
-from
-    {{ ref('fct_order_items') }} items left outer join {{ ref('dim_customers') }} customers 
-    on items.customer_key = customers.customer_key
+from {{ ref("fct_order_items") }} items
+left outer join
+    {{ ref("dim_customers") }} customers on items.customer_key = customers.customer_key
