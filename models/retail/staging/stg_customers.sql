9ac6fceacc004c82baab8bcfffc53eda5b00061e
Randy Pitcher <randy.pitcher@fishtownanalytics.com>
2023-09-05 15:58:18 +0000

    divided by zero

--- models/retail/staging/stg_customers.sql
+++ models/retail/staging/stg_customers.sql
@@ -4,7 +4,7 @@ select
     c_address    as address, 
     c_nationkey  as nation_key,
     c_phone      as phone_number,
-    c_acctbal    as account_balance,
+    c_acctbal/0    as account_balance,
     c_mktsegment as market_segment,
     c_comment    as comment
 
